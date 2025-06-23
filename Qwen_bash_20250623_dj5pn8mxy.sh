b73cf08a4b6d7c6d6b12b32eba71c2e52db1930c
AussieCoinVault <aussiecoinvault@outlook.com>
2025-06-23 12:29:09 +1000

    Update Qwen_bash_20250623_dj5pn8mxy.sh

--- Qwen_bash_20250623_dj5pn8mxy.sh
+++ Qwen_bash_20250623_dj5pn8mxy.sh
@@ -3,6 +3,6 @@ git init
 git add .
 git commit -m "Initial commit"
 
-git remote add origin https://github.com/YOUR_USERNAME/aussie-coin-vault.git 
+git remote add origin https://github.com/aussie-coin-vault/aussie-coin-vault.git 
 git branch -M main
 git push -u origin main
