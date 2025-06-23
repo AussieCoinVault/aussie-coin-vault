b060be9f1904d470727f9feb74054193b48606cf
AussieCoinVault <aussiecoinvault@outlook.com>
2025-06-23 12:24:05 +1000

    Add files via upload

---
+++ Qwen_bash_20250623_dj5pn8mxy.sh
@@ -0,0 +1,8 @@
+# In your local aussie-coin-vault folder
+git init
+git add .
+git commit -m "Initial commit"
+
+git remote add origin https://github.com/YOUR_USERNAME/aussie-coin-vault.git 
+git branch -M main
+git push -u origin main
